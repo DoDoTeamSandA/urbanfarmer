9c90d4b14836781d0ea9fec6195393d47082fa7d
DoDoTeamSandA <dodsolutions4@gmail.com>
2021-02-18 16:53:49 +0530

    Added Thoght Page to frebase

--- app/src/main/java/com/dodo/urbanfarmer/AddViewModel.java
+++ app/src/main/java/com/dodo/urbanfarmer/AddViewModel.java
@@ -5,10 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.MediaController;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddViewModel extends ViewModel {
 
@@ -16,12 +29,22 @@ public class AddViewModel extends ViewModel {
     Context context;
     MediaController mediaController;
     public MutableLiveData<Uri> throughtVideoUri;
+    public FirebaseFirestore firebaseFirestore;
+    public FirebaseAuth firebaseAuth;
+    public String Uid;
+    public Map<String,Object> thoughtMap=new HashMap<>();
+
+
+
 
 
     public AddViewModel(Context mcontext){
         context=mcontext;
         init();
         mediaController=new MediaController(context);
+        Uid=firebaseAuth.getInstance().getUid();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+
     }
 
     private void init() {
@@ -53,6 +76,33 @@ public class AddViewModel extends ViewModel {
     }
 
     public void uploadThoughData(){
+
+        thoughtMap.put("ThoughtText",thoghText);
+        thoughtMap.put("ThoughtPicURL",thoughtPicUrl);
+        thoughtMap.put("ThoughtNetwork",thoughtNetwork);
+        thoughtMap.put("AddHashTag",Addhashtag);
+        //thoughtMap.put("ThoughtVideoURL",throughtVideoUri);
+
+        firebaseFirestore.collection("ThoughtData").document(Uid).set(thoughtMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+
+                Log.e("ADDView","Sucess");
+                Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("AddViewError","Flonk");
+                Toast.makeText(context, "failure", Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
+
 
     }
 
